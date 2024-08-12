49663dfc75bbba1ed9173aea2d302f7fd7bd1f54
@05409790235 <andersonengsoft@gmail.com>
2024-08-12 08:20:28 -0400

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import { BrowserRouter } from "react-router-dom";
+import { AppRoutes } from "./routes";
 
 export const App= ()=>{
   return (
-    <div>
-      tyeste
-    </div>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   );
 }
 
